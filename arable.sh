ce7c9d5789170eb51811e8207bb07b3f262969a6
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2018-06-06 12:25:20 +0200

    arable.sh: implements [-F host..]: filter by hostname (processed after -H).

--- arable.sh
+++ arable.sh
@@ -42,18 +42,19 @@ rsync_push() {
 };
 
 main() {
-	local	_Hflag="" _lflag="" _nflag="" _tflag="" _xflag=0	\
-		_fun="" _funs="" _hname="" _hosts_line="" _opt=""	\
-		_script_fname="" _uname="";
-	while getopts hH:lnt:x _opt; do
+	local	_Fflag="" _Hflag="" _lflag="" _nflag="" _tflag="" _xflag=0	\
+		_fun="" _funs="" _hname="" _hosts_line="" _opt="" _script_fname="" _uname="";
+	while getopts hF:H:lnt:x _opt; do
 	case "${_opt}" in
+	F) _Fflag="$(echo "${OPTARG}" | sed 's/,/ /g')"; ;;
 	H) _Hflag="$(echo "${OPTARG}" | sed 's/,/ /g')"; ;;
 	l) _lflag=1; ;;
 	n) _nflag=1; ;;
 	t) _tflag="$(echo "${OPTARG}" | sed 's/,/ /g')"; ;;
 	x) _xflag=1; set -o xtrace; ;;
-	*) echo "usage: ${0} [-h] [-H host..] [-n] [-t tag..] [-x]" >&2;
+	*) echo "usage: ${0} [-h] [-F host..] [-H host..] [-n] [-t tag..] [-x]" >&2;
 	   echo "       -h.......: show this screen" >&2;
+	   echo "       -F host..: filter by hostname (processed after -H)" >&2;
 	   echo "       -H host..: limit by hostname" >&2;
 	   echo "       -n.......: perform dry run" >&2;
 	   echo "       -t tag...: limit by tag" >&2;
@@ -67,26 +68,29 @@ main() {
 		_funs="${_funs:+${_funs} }${_fun}";
 	done;
 	for _fun in ${_funs}; do
-		if [ -n "${_tflag}" ]					\
+		if [ -n "${_tflag}" ]						\
 		&& ! lsearch "${_tflag}" "${_fun#process_}"; then
 			continue;
 		else
 			msgf "[35;4m--- %s ---[0m" "$(eval echo "\${${_fun}_legend}")";
 		fi;
 		while read _hosts_line; do
-			if [ -z "${_hosts_line}" ]			\
+			if [ -z "${_hosts_line}" ]				\
 			|| [ "${_hosts_line#\#}" != "${_hosts_line}" ]; then
 				continue;
 			fi;
 			_uname="${_hosts_line%%@*}"; _hname="${_hosts_line##*@}";
-			if [ -n "${_uname}" ]				\
+			if [ -n "${_uname}" ]					\
 			&& [ -n "${_hname}" ]; then
-				if [ -n "${_Hflag}" ]			\
+				if [ -n "${_Hflag}" ]				\
 				&& ! lsearch "${_Hflag}" "${_hname}"; then
 					continue;
-				else
-					"${_fun}" "${_uname}" "${_hname}" "${_tflag}" "${_nflag}";
 				fi;
+				if [ -n "${_Fflag}" ]				\
+				&& lsearch "${_Fflag}" "${_hname}"; then
+					continue;
+				fi;
+				"${_fun}" "${_uname}" "${_hname}" "${_tflag}" "${_nflag}";
 			fi;
 		done < "./hosts";
 	done;
