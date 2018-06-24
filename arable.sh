9df07c307705bdd0b0a80d7236dc1c2874a35fd6
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2018-06-24 12:36:31 +0200

    arable.sh:main(): split off usage() & exit w/ correct status.

--- arable.sh
+++ arable.sh
@@ -41,24 +41,28 @@ rsync_push() {
 		${_src} "${_uname}@${_hname}:${_dest}";
 };
 
+usage() {
+	echo "usage: ${0} [-h] [-F host..] [-H host..] [-n] [-t tag..] [-x]" >&2;
+	echo "       -h.......: show this screen" >&2;
+	echo "       -F host..: filter by hostname (processed after -H)" >&2;
+	echo "       -H host..: limit by hostname" >&2;
+	echo "       -n.......: perform dry run" >&2;
+	echo "       -t tag...: limit by tag" >&2;
+	echo "       -x.......: enable xtrace debugging" >&2;
+};
+
 main() {
 	local	_Fflag="" _Hflag="" _nflag="" _tflag="" _xflag=0	\
 		_fun="" _funs="" _hname="" _hosts_line="" _opt="" _script_fname="" _uname="";
 	while getopts hF:H:nt:x _opt; do
 	case "${_opt}" in
 	F) _Fflag="$(echo "${OPTARG}" | sed 's/,/ /g')"; ;;
+	h) usage; exit 0; ;;
 	H) _Hflag="$(echo "${OPTARG}" | sed 's/,/ /g')"; ;;
 	n) _nflag=1; ;;
 	t) _tflag="$(echo "${OPTARG}" | sed 's/,/ /g')"; ;;
 	x) _xflag=1; set -o xtrace; ;;
-	*) echo "usage: ${0} [-h] [-F host..] [-H host..] [-n] [-t tag..] [-x]" >&2;
-	   echo "       -h.......: show this screen" >&2;
-	   echo "       -F host..: filter by hostname (processed after -H)" >&2;
-	   echo "       -H host..: limit by hostname" >&2;
-	   echo "       -n.......: perform dry run" >&2;
-	   echo "       -t tag...: limit by tag" >&2;
-	   echo "       -x.......: enable xtrace debugging" >&2;
-	   exit 0; ;;
+	*) usage; exit 1; ;;
 	esac; done;
 	for _script_fname in $(set +o noglob; echo tasks/*.sh); do
 		. "${_script_fname}";
